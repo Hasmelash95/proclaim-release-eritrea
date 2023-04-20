29db5554a1b0fc588a3e23ca5cbd0da7b33dde28
Hasmelash95 <103432143+Hasmelash95@users.noreply.github.com>
2023-04-20 21:14:45 +0000

    Make changes to fond sizes

--- release-eritrea/settings.py
+++ release-eritrea/settings.py
@@ -116,8 +116,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'release-eritrea.wsgi.application'
 
-CLOUDINARY_URL = os.environ.get('CLOUDINARY_URL')
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
