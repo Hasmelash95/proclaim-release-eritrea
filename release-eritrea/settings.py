8abd76173a2e6b6a34aab52cd91d8d713a58abd3
Hasmelash95 <103432143+Hasmelash95@users.noreply.github.com>
2023-06-05 14:14:22 +0000

    Fix allowed hosts name

--- release-eritrea/settings.py
+++ release-eritrea/settings.py
@@ -35,7 +35,7 @@ DEBUG = False
 ALLOWED_HOSTS = [
     'releaseeritrea.org',
     'proclaim-release-eritrea.onrender.com',
-    'proclaim-release-eritrea.heroku.com',
+    'proc-release-eritrea.heroku.com',
     'localhost',
     '127.0.0.1',
     '0.0.0.0'
